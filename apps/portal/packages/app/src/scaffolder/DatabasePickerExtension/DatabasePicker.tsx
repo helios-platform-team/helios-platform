6ba077f087f62ba9a9603676c62b78281b41203e
NgocAnhDo26 <dotanngocanh@gmail.com>
2026-03-04 23:34:04 +0700

    feat(scaffolder): add DatabasePicker extension for database selection

---
+++ apps/portal/packages/app/src/scaffolder/DatabasePickerExtension/DatabasePicker.tsx
@@ -0,0 +1,14 @@
+import { FieldExtensionComponentProps } from '@backstage/plugin-scaffolder-react';
+
+export const DatabasePicker = ({
+  onChange,
+  rawErrors,
+  required,
+  formData,
+  uiSchema,
+  schema,
+  formContext,
+}: FieldExtensionComponentProps<string>) => {
+  // Todo: Implementation
+  return <></>;
+};
