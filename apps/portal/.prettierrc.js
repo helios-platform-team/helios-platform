8d9cc32672ec1f746e448b82c4939119b897406a
NgocAnhDo26 <dotanngocanh@gmail.com>
2026-03-10 21:35:24 +0700

    refactor(portal): update Prettier configuration and clean up code
    - Removed the .prettierrc.js file and integrated Prettier configuration into package.json.
    - Updated .prettierignore to exclude examples, dist-types, and docs directories.
    - Cleaned up code formatting in various files, including Root.tsx and UserHeader.tsx, for improved readability.
    - Enhanced YAML configuration for GitHub actions and Kubernetes actions with better formatting and comments.

--- apps/portal/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ...require('@backstage/cli/config/prettier'),
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
-  importOrder: ['^@backstage/(.*)$', '<THIRD_PARTY_MODULES>', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-};
